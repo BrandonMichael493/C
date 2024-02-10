4cebbe5df65e838686415cdd44c56ff0074dfedd
Brandon Hernandez <Brandonmichael493@gmail.com>
2024-02-09 19:03:41 -0500

    first commit

---
+++ boverflowexample.c
@@ -0,0 +1,11 @@
+int main(int argc , char *argv []) {
+    int valid = 0;
+    char str1 [8];
+    char str2 [8];
+    strcpy(str1 , "START");
+    gets(str2);
+    if (strncmp(str1 , str2 , 8) == 0)
+        valid = 1;
+    printf("buffer1: str1(%s), str2(%s), valid (%d)\n", str1 , str2 , valid);
+    //printf("%s", &valid)
+}
